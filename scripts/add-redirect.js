6d58a735da9fc13053f9c065912abe4ba72cc866
sych74 <sych74@gmail.com>
2017-06-21 13:32:35 +0200

    Update add-redirect.js

--- scripts/add-redirect.js
+++ scripts/add-redirect.js
@@ -6,7 +6,7 @@ var envName = '${env.envName}';
 var resp = jelastic.env.control.AddEndpoint(envName, session, nodeId, port, "TCP", "Node-RED WS");
 if (resp.result != 0) return resp;
 
-var redirect = "//redirect from 80->1880\nrequire('http').createServer(function (req, res) { res.writeHead(301, {'Location': 'http://' + req.headers.host + ':'" + 1000 + " + req.url}); res.end(); }).listen(7654);\n//----\nvar http "
+var redirect = "require('http').createServer(function (req, res) { res.writeHead(301, {'Location': 'http://' + req.headers.host + ':'" + resp.object.publicPort + " + req.url}); res.end(); }).listen(7654, '0.0.0.0');"
 
-resp = jelastic.env.file.ReplaceInBody(envName, session, "/usr/src/node-red/node_modules/node-red/red.js", "var http ", redirect, 1, null, "cp", true, nodeId);
+resp = jelastic.env.file.ReplaceInBody(envName, session, "/usr/src/node-red/node_modules/node-red/red.js", "var http = ", "require('./redirect.js'); var http = ", 1, null, "cp", true, nodeId);
 return resp;
