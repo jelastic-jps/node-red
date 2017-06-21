860bec3124b0793fa9cb5eadb7472403e8a2ff4c
sych74 <sych74@gmail.com>
2017-06-21 14:27:08 +0200

    Update add-redirect.js

--- scripts/add-redirect.js
+++ scripts/add-redirect.js
@@ -6,7 +6,10 @@ var envName = '${env.envName}';
 var resp = jelastic.env.control.AddEndpoint(envName, session, nodeId, port, "TCP", "Node-RED WS");
 if (resp.result != 0) return resp;
 
-var redirect = "require('http').createServer(function (req, res) { res.writeHead(301, {'Location': 'http://' + req.headers.host + ':'" + resp.object.publicPort + " + req.url}); res.end(); }).listen(7654, '0.0.0.0');"
+var destPort = resp.object.publicPort;
+var redirPort = 7654; 
+var redirect = "require(\"http\").createServer(function (req, res) { res.writeHead(301, {\"Location\": \"http://\" + req.headers.host + \":" + destPort + "\" + req.url}); res.end(); }).listen(" + redirPort + ", \"0.0.0.0\");"
 
-resp = jelastic.env.file.ReplaceInBody(envName, session, "/usr/src/node-red/node_modules/node-red/red.js", "var http = ", "require('./redirect.js'); var http = ", 1, null, "cp", true, nodeId);
+path = "/usr/src/node-red/node_modules/node-red/red.js";
+resp = jelastic.env.file.ReplaceInBody(envName, session, path, "var http = ", redirect + "\nvar http = ", 1, null, null, true, nodeId);
 return resp;
